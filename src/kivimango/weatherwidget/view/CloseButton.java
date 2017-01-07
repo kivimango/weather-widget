7ceb7a7d22ccdfb9e1271a896243c02b27cb67f2
kivimango <dev@c0d3r.nhely.hu>
2017-01-07 13:51:40 +0100

    Added CloseButton icon

--- src/kivimango/weatherwidget/view/CloseButton.java
+++ src/kivimango/weatherwidget/view/CloseButton.java
@@ -1,27 +1,33 @@
-/**
- * Setting up the fully transparent Close button in the top right corner.
- * Clicking on this button the application will exit with the code 0.
- * 
- * @author		kivimango	dev@kivimango.hu
- * @copyright	kivimango	
- * @license		MIT License	https://opensource.org/licenses/MIT
- * @link 		https://github.com/kivimango/weather-widget
- * @version		0.1
- * @since		0.1
- * @package 	kivimango.weatherwidget.view
- */
-
 package kivimango.weatherwidget.view;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
+/**
+ * Setting up the fully transparent Close button in the top right corner.
+ * Clicking on this button the application will exit with the code 0.
+ * 
+ * @author		kivimango	dev@kivimango.hu
+ * @copyright	kivimango	
+ * @license		GNU General Public License v3	https://www.gnu.org/licenses/gpl-3.0.html
+ * @link 		https://github.com/kivimango/weather-widget
+ * @version		0.1
+ * @since		0.1
+ * @package 	kivimango.weatherwidget.view
+ */
 
 public class CloseButton extends JButton implements ActionListener {
 	
 	private static final long serialVersionUID = 3618737918701167479L;
+	private BufferedImage buttonIcon;
 
 	public CloseButton() {
 		super();
@@ -30,14 +36,25 @@ public class CloseButton extends JButton implements ActionListener {
 		setContentAreaFilled(false);
 		setFont(new Font("SansSerif", Font.BOLD, 18));
 		setForeground(Color.WHITE);
+		seticonFromResource();
 		setOpaque(false);
-		setText("X");
+		setToolTipText("Closes the application");
 		addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		 System.exit(0);
+	}
+	
+	private void seticonFromResource()
+	{
+		try {
+			buttonIcon = ImageIO.read(getClass().getResource("/buttons/close.png"));;
+			setIcon(new ImageIcon(buttonIcon));
+		} catch (IOException | IllegalArgumentException e) {
+			// TO-DO : Implement some warning to the user, eg.:broken app, missing file, resinstall it, etc
+		}
 	}
 
 }
